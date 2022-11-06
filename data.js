142eb85d1c3db53d0d657e74aad741b60c611a38
17111150 <17111150@suning.com>
2022-11-06 19:35:18 +0800

    1

--- data.js
+++ data.js
@@ -1,68 +1,52 @@
 var imageData = [
-  {
-    "name":"dongzao",
-    "en":"Dates",
-    "pic":"01dongzao_pic",
-    "eeg":"01dongzao_eeg"
-  },
-  {
-    "name":"meigui",
-    "en":"Roses",
-    "pic":"02meigui_pic",
-    "eeg":"02meigui_eeg"
-  },
-  {
-    "name":"ningmeng",
-    "en":"Lemon",
-    "pic":"03ningmeng_pic",
-    "eeg":"03ningmeng_eeg"
-  },
-  {
-    "name":"fendiye",
-    "en":"foundation",
-    "pic":"04fendiye_pic",
-    "eeg":"04fendiye_eeg"
-  },
-  {
-    "name":"yanying",
-    "en":"eyeshadow",
-    "pic":"05yanying_pic",
-    "eeg":"05yanying_eeg"
-  },
-  {
-    "name":"hushoushuang",
-    "en":"handcream",
-    "pic":"06hushoushuang_pic",
-    "eeg":"06hushoushuang_eeg"
-  },
-  {
-    "name":"muxiang",
-    "en":"wood",
-    "pic":"07muxiang_pic",
-    "eeg":"07muxiang_eeg"
-  },
-  {
-    "name":"yasi",
-    "en":"IELTS",
-    "pic":"08yasi_pic",
-    "eeg":"08yasi_eeg"
-  },
-  {
-    "name":"weijin",
-    "en":"scarf",
-    "pic":"09weijin_pic",
-    "eeg":"09weijin_eeg"
-  },
-  {
-    "name":"jiujing",
-    "en":"alcohol",
-    "pic":"10jiujing_pic",
-    "eeg":"10jiujing_eeg"
-  },
-  {
-    "name":"kafei",
-    "en":"coffee",
-    "pic":"11kafei_pic",
-    "eeg":"11kafei_eeg"
-  },
-]
+  {name:"dongzao",en:"	Winter Jujube	",pic:"01dongzao_pic"	,eeg:"01dongzao_eeg"},
+  {name:"meigui",en:"	Roses	",pic:"02meigui_pic"	,eeg:"02meigui_eeg"},
+  {name:"ningmeng",en:"	Lemon	",pic:"03ningmeng_pic"	,eeg:"03ningmeng_eeg"},
+  {name:"fendiye",en:"	Foundation	",pic:"04fendiye_pic"	,eeg:"04fendiye_eeg"},
+  {name:"yanying",en:"	Eyeshadow	",pic:"05yanying_pic"	,eeg:"05yanying_eeg"},
+  {name:"hushoushuang",en:"	Hand Cream	",pic:"06hushoushuang_pic"	,eeg:"06hushoushuang_eeg"},
+  {name:"muxiang",en:"	Wood	",pic:"07muxiang_pic"	,eeg:"07muxiang_eeg"},
+  {name:"yasi",en:"	IELTS	",pic:"08yasi_pic"	,eeg:"08yasi_eeg"},
+  {name:"weijin",en:"	Scarf	",pic:"09weijin_pic"	,eeg:"09weijin_eeg"},
+  {name:"jiujing",en:"	Alcohol	",pic:"10jiujing_pic"	,eeg:"10jiujing_eeg"},
+  {name:"kafei",en:" 	Coffee	",pic:"11kafei_pic"	,eeg:"11kafei_eeg"},
+  {name:"guihua",en:"	Sweet Osmanthus	",pic:"12guihua_pic"	,eeg:"12guihua_eeg"},
+  {name:"maomi",en:"	Cat	",pic:"13maomi_pic"	,eeg:"13maomi_eeg"},
+  {name:"qiaokeli",en:"	Chocolates	",pic:"14qiaokeli_pic"	,eeg:"14kafei_eeg"},
+  {name:"shala",en:"	Salad	",pic:"15shala_pic"	,eeg:"15shala_eeg"},
+  {name:"baozhi",en:"	Newspaper	",pic:"16baozhi_pic"	,eeg:"16baozhi_eeg"},
+  {name:"hongjiu",en:"	Wine",pic:"17hongjiu_pic"	,eeg:"17hongjiu_eeg"},
+  {name:"jiemo",en:"	Mustard	",pic:"18jiemo_pic"	,eeg:"18jiemo_eeg"},
+  {name:"kouhong",en:"	Lipstick	",pic:"19kouhong_pic"	,eeg:"19kouhong_eeg"},
+  {name:"benzi",en:"	Notebook	",pic:"20benzi_pic"	,eeg:"20benzi_eeg"},
+  {name:"dizhonghai",en:"	Mediterranean Perfume	",pic:"21dizhonghai_pic"	,eeg:"21dizhonghai_eeg"},
+  {name:"binggan",en:"	Cookie	",pic:"22binggan_pic"	,eeg:"22binggan_eeg"},
+  {name:"canju",en:"	Knife & Fork	",pic:"23canju_pic"	,eeg:"23canju_eeg"},
+  {name:"shuiguodangao",en:"	Fruit cake	",pic:"24shuiguodangao_pic"	,eeg:"24shuiguodangao_eeg"},
+  {name:"jichuan",en:"	Chicken Skewer	",pic:"25jichuan_pic"	,eeg:"25jichuan_eeg"},
+  {name:"lajiao",en:"	Hot Pepper	",pic:"26lajiao_pic"	,eeg:"26lajiao_eeg"},
+  {name:"xingdailu",en:"	StellaLou	",pic:"27xingdailu_pic"	,eeg:"27xingdailu_eeg"},
+  {name:"yanliao",en:"	Pigment	",pic:"28yanliao_pic"	,eeg:"28yanliao_eeg"},
+  {name:"zhaxia",en:"	Fried Praw	",pic:"29zhaxia_pic"	,eeg:"29zhaxia_eeg"},
+  {name:"gutou",en:"	Bone】	",pic:"30gutou_pic"	,eeg:"30gutou_eeg"},
+  {name:"chaye",en:"	Tea	",pic:"31chaye_pic"	,eeg:"31chaye_eeg"},
+  {name:"mihoutao",en:"	Kiwifruit	",pic:"32mihoutao_pic"	,eeg:"32mihoutao_eeg"},
+  {name:"miantiao",en:"	Noodles	",pic:"33miantiao_pic"	,eeg:"33miaotiao_eeg"},
+  {name:"huashu",en:"	Bouquet	",pic:"34huashu_pic"	,eeg:"34huashu_eeg"},
+  {name:"mofang",en:"	Magic square	",pic:"35mofang_pic"	,eeg:"35mofang_eeg"},
+  {name:"suliaobu",en:"	Plastic Cloth	",pic:"36suliaobu_pic"	,eeg:"36suliaobu_eeg"},
+  {name:"wanjutuzi",en:"	Rabbit toy	",pic:"37wanjutuzi_pic"	,eeg:"37wanjutuzi_eeg"},
+  {name:"jiangpai",en:"	Medal	",pic:"38jiangpai_pic"	,eeg:"38jiangpai_eeg"},
+  {name:"lvzhi",en:"	Green Plants	",pic:"39lvzhi_pic"	,eeg:"39lvzhi_eeg"},
+  {name:"shanhu",en:"	Coral	",pic:"40shanhu_pic"	,eeg:"40shanhu_eeg"},
+  {name:"shengjing",en:"	Bible	",pic:"41shengjing_pic"	,eeg:"41shengjing_eeg"},
+  {name:"yagao",en:"	Toothpaste	",pic:"42yagao_pic"	,eeg:"42yagao_eeg"},
+  {name:"yinhangka",en:"	Bank Card	",pic:"43yinhangka_pic"	,eeg:"43yinhangka_eeg"},
+  {name:"kekoukele",en:"	Coca Cola	",pic:"44kekoukele_pic"	,eeg:"44kekoukele_eeg"},
+  {name:"xiyiye",en:"	Washing Liquid	",pic:"45xiyiye_pic"	,eeg:"45xiyiye_eeg"},
+  {name:"tusi",en:"	Toast	",pic:"46tusi_pic"	,eeg:"46tusi_eeg"},
+  {name:"shousi",en:"	Sushi	",pic:"47shousi_pic"	,eeg:"47shousi_eeg"},
+  {name:"haiyan",en:"	Sea salt	",pic:"48haiyan_pic"	,eeg:"48haiyan_eeg"},
+  {name:"pinggiuo",en:"	Apple	",pic:"49pingguo_pic"	,eeg:"49pingguo_eeg"},
+  {name:"yanyaoshui",en:"	Eye Drops	",pic:"50yanyaoshui_pic"	,eeg:"50yanyaoshui_eeg"},
+  ]
